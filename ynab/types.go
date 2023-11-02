56705db5eb0022813b4e879130ac27b7b5a08ce9
Aaron Ellington <aaron@ellington.io>
2023-11-02 10:47:12 -0400

    complete budget types

--- ynab/types.go
+++ ynab/types.go
@@ -3,14 +3,24 @@ package ynab
 import "time"
 
 type (
-	AccountID       string
-	AccountType     string
-	BudgetID        string
-	CategoryGroupID string
-	CategoryID      string
-	PayeeID         string
-	PayeeLocationID string
-	GoalType        string
+	AccountID                 string
+	AccountType               string
+	Milliunits                int64
+	BudgetID                  string
+	CategoryGroupID           string
+	CategoryID                string
+	DebtTransactionType       string
+	FlagColor                 string
+	GoalType                  string
+	ImportID                  string
+	PayeeID                   string
+	PayeeLocationID           string
+	ScheduledSubTransactionID string
+	ScheduledTransactionID    string
+	ScheduleFrequency         string
+	SubTransactionID          string
+	TransactionID             string
+	TransactionStatus         string
 )
 
 const (
@@ -37,6 +47,48 @@ const (
 	GoalTypeDebt                        GoalType = "DEBT"
 )
 
+const (
+	TransactionStatusCleared    TransactionStatus = "cleared"
+	TransactionStatusUncleared  TransactionStatus = "uncleared"
+	TransactionStatusReconciled TransactionStatus = "reconciled"
+)
+
+const (
+	FlagColorRed    FlagColor = "red"
+	FlagColorOrange FlagColor = "orange"
+	FlagColorYellow FlagColor = "yellow"
+	FlagColorGreen  FlagColor = "green"
+	FlagColorBlue   FlagColor = "blue"
+	FlagColorPurple FlagColor = "purple"
+)
+
+const (
+	DebtTransactionTypePayment           DebtTransactionType = "payment"
+	DebtTransactionTypeRefund            DebtTransactionType = "refund"
+	DebtTransactionTypeFee               DebtTransactionType = "fee"
+	DebtTransactionTypeInterest          DebtTransactionType = "interest"
+	DebtTransactionTypeEscrow            DebtTransactionType = "escrow"
+	DebtTransactionTypeBalanceAdjustment DebtTransactionType = "balanceAdjustment"
+	DebtTransactionTypeCredit            DebtTransactionType = "credit"
+	DebtTransactionTypeCharge            DebtTransactionType = "charge"
+)
+
+const (
+	ScheduleFrequencyNever           ScheduleFrequency = "never"
+	ScheduleFrequencyDaily           ScheduleFrequency = "daily"
+	ScheduleFrequencyWeekly          ScheduleFrequency = "weekly"
+	ScheduleFrequencyEveryOtherWeek  ScheduleFrequency = "everyOtherWeek"
+	ScheduleFrequencyTwiceAMonth     ScheduleFrequency = "twiceAMonth"
+	ScheduleFrequencyEvery4Weeks     ScheduleFrequency = "every4Weeks"
+	ScheduleFrequencyMonthly         ScheduleFrequency = "monthly"
+	ScheduleFrequencyEveryOtherMonth ScheduleFrequency = "everyOtherMonth"
+	ScheduleFrequencyEvery3Months    ScheduleFrequency = "every3Months"
+	ScheduleFrequencyEvery4Months    ScheduleFrequency = "every4Months"
+	ScheduleFrequencyTwiceAYear      ScheduleFrequency = "twiceAYear"
+	ScheduleFrequencyYearly          ScheduleFrequency = "yearly"
+	ScheduleFrequencyEveryOtherYear  ScheduleFrequency = "everyOtherYear"
+)
+
 type BudgetSummaryResponse struct {
 	Data BudgetSummaryResponseData `json:"data"`
 }
@@ -57,10 +109,15 @@ type BudgetDetailResponseData struct {
 
 type BudgetDetail struct {
 	BudgetSummary
-	Payees         []Payee         `json:"payees"`
-	PayeeLocations []PayeeLocation `json:"payee_locations"`
-	CategoryGroups []CategoryGroup `json:"category_groups"`
-	Categories     []Category      `json:"categories"`
+	Payees                   []Payee                       `json:"payees"`
+	PayeeLocations           []PayeeLocation               `json:"payee_locations"`
+	CategoryGroups           []CategoryGroup               `json:"category_groups"`
+	Categories               []Category                    `json:"categories"`
+	Months                   []MonthDetail                 `json:"months"`
+	Transactions             []TransactionSummary          `json:"transactions"`
+	SubTransactions          []SubTransaction              `json:"subtransactions"`
+	ScheduledTransactions    []ScheduledTransactionSummary `json:"scheduled_transactions"`
+	ScheduledSubTransactions []ScheduledSubTransaction     `json:"scheduled_subtransactions"`
 }
 
 type BudgetSummary struct {
@@ -81,14 +138,14 @@ type Account struct {
 	OnBudget            bool                      `json:"on_budget"`
 	Closed              bool                      `json:"closed"`
 	Note                *string                   `json:"note"`
-	Balance             int64                     `json:"balance"`
-	ClearedBalance      int64                     `json:"cleared_balance"`
-	UnclearedBalance    int64                     `json:"uncleared_balance"`
+	Balance             Milliunits                `json:"balance"`
+	ClearedBalance      Milliunits                `json:"cleared_balance"`
+	UnclearedBalance    Milliunits                `json:"uncleared_balance"`
 	TransferPayeeID     *PayeeID                  `json:"transfer_payee_id"`
 	DirectImportLinked  bool                      `json:"direct_import_linked"`
 	DirectImportInError bool                      `json:"direct_import_in_error"`
 	LastReconciledAt    *time.Time                `json:"last_reconciled_at"`
-	DebtOriginalBalance *int64                    `json:"debt_original_balance"`
+	DebtOriginalBalance *Milliunits               `json:"debt_original_balance"`
 	DebtInterestRates   *LoadAccountPeriodicValue `json:"debt_interest_rates"`
 	DebtMinimumPayments *LoadAccountPeriodicValue `json:"debt_minimum_payments"`
 	DebtEscrowAmounts   *LoadAccountPeriodicValue `json:"debt_escrow_amounts"`
@@ -124,22 +181,95 @@ type Category struct {
 	Name                   string          `json:"name"`
 	Hidden                 bool            `json:"hidden"`
 	Note                   string          `json:"note"`
-	Budgeted               int64           `json:"budgeted"`
-	Activity               int64           `json:"activity"`
-	Balance                int64           `json:"balance"`
+	Budgeted               Milliunits      `json:"budgeted"`
+	Activity               Milliunits      `json:"activity"`
+	Balance                Milliunits      `json:"balance"`
 	GoalType               *GoalType       `json:"goal_type"`
 	GoalDay                *int32          `json:"goal_day"`
 	GoalCadence            *int32          `json:"goal_cadence"`
 	GoalCadenceFrequency   *int32          `json:"goal_cadence_frequency"`
 	GoalCreationMonth      *string         `json:"goal_creation_month"`
-	GoalTarget             *int64          `json:"goal_target"`
+	GoalTarget             *Milliunits     `json:"goal_target"`
 	GoalTargetMonth        *string         `json:"goal_target_month"`
 	GoalPercentageComplete *int32          `json:"goal_percentage_complete"`
 	GoalMonthsToBudget     *int32          `json:"goal_months_to_budget"`
-	GoalUnderFunded        *int64          `json:"goal_under_funded"`
-	GoalOverallFunded      *int64          `json:"goal_overall_funded"`
-	GoalOverallLeft        *int64          `json:"goal_overall_left"`
+	GoalUnderFunded        *Milliunits     `json:"goal_under_funded"`
+	GoalOverallFunded      *Milliunits     `json:"goal_overall_funded"`
+	GoalOverallLeft        *Milliunits     `json:"goal_overall_left"`
 	Deleted                bool            `json:"deleted"`
+}
+
+type MonthDetail struct {
+	Month        string     `json:"month"`
+	Note         *string    `json:"note"`
+	Income       Milliunits `json:"income"`
+	Budgeted     Milliunits `json:"budgeted"`
+	Activity     Milliunits `json:"activity"`
+	ToBeBudgeted Milliunits `json:"to_be_budgeted"`
+	AgeOfMoney   *int32     `json:"age_of_money"`
+	Deleted      bool       `json:"deleted"`
+	Categories   []Category `json:"categories"`
+}
+
+type TransactionSummary struct {
+	ID                      TransactionID        `json:"id"`
+	Date                    string               `json:"date"`
+	Amount                  Milliunits           `json:"amount"`
+	Memo                    *string              `json:"memo"`
+	TransactionStatus       TransactionStatus    `json:"cleared"`
+	Approved                bool                 `json:"approved"`
+	FlagColor               FlagColor            `json:"flag_color"`
+	AccountID               AccountID            `json:"account_id"`
+	PayeeID                 *PayeeID             `json:"payee_id"`
+	CategoryID              *CategoryID          `json:"category_id"`
+	TransferAccountID       *AccountID           `json:"transfer_account_id"`
+	TransferTransactionID   *TransactionID       `json:"transfer_transaction_id"`
+	MatchedTransactionID    *TransactionID       `json:"matched_transaction_id"`
+	ImportID                *ImportID            `json:"import_id"`
+	ImportPayeeName         *string              `json:"import_payee_name"`
+	ImportPayeeNameOriginal *string              `json:"import_payee_name_original"`
+	DebtTransactionType     *DebtTransactionType `json:"debt_transaction_type"`
+	Deleted                 bool                 `json:"deleted"`
+}
+
+type SubTransaction struct {
+	ID                    SubTransactionID `json:"id"`
+	TransactionID         TransactionID    `json:"transaction_id"`
+	Amount                Milliunits       `json:"amount"`
+	Memo                  *string          `json:"memo"`
+	PayeeID               *PayeeID         `json:"payee_id"`
+	PayeeName             *string          `json:"payee_name"`
+	CategoryID            *CategoryID      `json:"category_id"`
+	CategoryName          *string          `json:"category_name"`
+	TransferAccountID     *AccountID       `json:"transfer_account_id"`
+	TransferTransactionID *TransactionID   `json:"transfer_transaction_id"`
+	Deleted               bool             `json:"deleted"`
+}
+
+type ScheduledTransactionSummary struct {
+	ID                ScheduledTransactionID `json:"id"`
+	DateFirst         string                 `json:"date_first"`
+	DateNext          string                 `json:"date_next"`
+	Frequency         ScheduleFrequency      `json:"frequency"`
+	Amount            Milliunits             `json:"amount"`
+	Memo              *string                `json:"memo"`
+	FlagColor         FlagColor              `json:"flag_color"`
+	AccountID         AccountID              `json:"account_id"`
+	PayeeID           *PayeeID               `json:"payee_id"`
+	CategoryID        *CategoryID            `json:"category_id"`
+	TransferAccountID *AccountID             `json:"transfer_account_id"`
+	Deleted           bool                   `json:"deleted"`
+}
+
+type ScheduledSubTransaction struct {
+	ID                     ScheduledSubTransactionID `json:"id"`
+	ScheduledTransactionID ScheduledTransactionID    `json:"scheduled_transaction_id"`
+	Amount                 Milliunits                `json:"amount"`
+	Memo                   *string                   `json:"memo"`
+	PayeeID                *PayeeID                  `json:"payee_id"`
+	CategoryID             *CategoryID               `json:"category_id"`
+	TransferAccountID      *AccountID                `json:"transfer_account_id"`
+	Deleted                bool                      `json:"deleted"`
 }
 
 type DateFormat struct {
